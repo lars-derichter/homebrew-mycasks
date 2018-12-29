65e12c1e8ae8cb6001a661b5434daae25bfb1720
Lars De Richter <lars.derichter@gmail.com>
2018-12-29 20:50:26 +0100

    Added version 3of camtasia based on: https://github.com/Homebrew/homebrew-cask/blob/5a23906625b69038ff62846fec234068b4efd013/Casks/camtasia.rb

---
+++ Casks/camtasia-3.rb
@@ -0,0 +1,10 @@
+cask 'camtasia-3' do
+  version '3.1.5'
+  sha256 'e0e0b1e879f10cb164b09aaa8eb97b22d9947751cb09f775a5187cbdcc5d2a0e'
+
+  url "https://download.techsmith.com/camtasiamac/enu/#{version.no_dots}/camtasia.dmg"
+  name 'Camtasia'
+  homepage 'https://www.techsmith.com/camtasia.html'
+
+  app "Camtasia #{version.major}.app"
+end
